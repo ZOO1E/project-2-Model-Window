c2377fbc022650ce3f5be1cef6501fccb808c536
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-27 18:19:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 'use strict';
-
-// html에서 불러오는 것이 귀찮기에
-const modal = document.querySelector('.modal');//html에서 hidden은 추가적인 것으로 제외해도 ㄱㅊ
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal =  document.querySelectorAll('.show-modal');
-
-
-//stop ! duplicate function
-const closemodal =function(){
-    modal.classList.add('hidden');//hidden추가
-    overlay.classList.add('hidden');
+ const modal = document.querySelector('.modal');
+ const showModal =  document.querySelectorAll('.show-modal');
+ const overlayEl =  document.querySelector('.overlay');
+ console.log(showModal);
+const closeModel =  document.querySelector('.close-modal')
+let i =0;
+const CloseModal = function(){ 
+    modal.classList.add('hidden');
+overlayEl.classList.add('hidden');
 }
-const openmodal = function(){
-    modal.classList.remove('hidden');//hidden제거로 창이 나옴
-    overlay.classList.remove('hidden');
-    }
+const Openmodal = function(){
+    modal.classList.remove('hidden');
+    overlayEl.classList.remove('hidden');
+}
+for(i=0; i<3 ; i++){ 
+showModal[i].addEventListener('click' , function(){
+    Openmodal();
+ })};
 
-btnsOpenModal.addEventListener('click',openmodal)
-btnCloseModal.addEventListener('click' , closemodal)
-overlay.addEventListener('click',closemodal)
+closeModel.addEventListener('click' , function(){
+    CloseModal();
+  });
 
-document.addEventListener('keydown' ,function (e){
-    if (e.key ==='Escape' && !modal.classList.contains('hidden'))
-     closemodal();
-})
+  overlayEl.addEventListener('click' , function(){
+  CloseModal();
+  })
 
-
-
-
+  document.addEventListener('keydown' , function(e){
+if(e.key === 'Escape' && !modal.classList.contains('hidden') ){
+    CloseModal();
+}
+  });
